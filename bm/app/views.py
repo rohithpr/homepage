b3bd7670bb12d27bf9f94cbc7583103640b9817a
rohithpr <praroh2@gmail.com>
2015-01-10 18:03:04 +0530

    Refactor. get_bookmarks()

--- bm/app/views.py
+++ bm/app/views.py
@@ -5,11 +5,37 @@ from django.contrib.auth import authenticate, login, logout
 
 from app.models import Category, Bookmark
 
-@login_required(login_url='/b/login')
-def home_page(request):
-	categories = Category.objects.filter(user=request.user)
-	bookmarks = {}
+############################################################
+# SESSION STUFF
+############################################################
 
+def login_page(request):
+	return render(request, 'login.html')
+
+def verify(request):
+	username = request.POST['username']
+	password = request.POST['password']
+	user = authenticate(username=username, password=password)
+	if user is not None:
+		if user.is_active:
+			login(request, user)
+			return HttpResponseRedirect('/')
+		else:
+			return HttpResponseRedirect('/b/login/')
+	else:
+		return HttpResponseRedirect('/b/login/')
+
+def logout_page(request):
+	logout(request)
+	return HttpResponseRedirect('/')
+
+############################################################
+# HELPERS
+############################################################
+
+def get_bookmarks(request):
+	categories = Category.objects.filter(user=request.user)
+	# 6 columns only
 	raw_col_0 = categories.filter(column_number=0).order_by('row_number')
 	col_0 = []
 	for category in raw_col_0:
@@ -39,38 +65,17 @@ def home_page(request):
 	col_5 = []
 	for category in raw_col_5:
 		col_5.append([category, Bookmark.objects.filter(category=category)])
-	
 
+	return [col_0, col_1, col_2, col_3, col_4, col_5]
 
+############################################################
+# MAIN
+############################################################
+
+@login_required(login_url='/b/login')
+def home_page(request):
 	context = {
 				'user': request.user,
-				'categories': categories,
-				'columns': [col_0, col_1, col_2, col_3, col_4, col_5], 
-				# 'col_0': col_0, 
-				# 'col_1': col_1,
-				# 'col_2': col_2, 
-				# 'col_3': col_3, 
-				# 'col_4': col_4, 
-				# 'col_5': col_5,
+				'columns': get_bookmarks(request),
 			}
-	# print(context)
 	return render(request, 'home.html', context)
-
-def login_page(request):
-	return render(request, 'login.html')
-
-def verify(request):
-	username = request.POST['username']
-	password = request.POST['password']
-	user = authenticate(username=username, password=password)
-	if user is not None:
-		if user.is_active:
-			login(request, user)
-			print("*** Logged in: ", user)
-			return HttpResponseRedirect('/')
-	else:
-		return HttpResponseRedirect('/b/login/')
-
-def logout_page(request):
-	logout(request)
-	return HttpResponseRedirect('/')
