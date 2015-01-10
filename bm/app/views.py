c85845ad16ac18b2c7a6e0e9839d93a8798b8383
rohithpr <praroh2@gmail.com>
2015-01-10 11:01:14 +0530

    Homepage skeleton

--- bm/app/views.py
+++ bm/app/views.py
@@ -9,10 +9,50 @@ from app.models import Category, Bookmark
 def home_page(request):
 	categories = Category.objects.filter(user=request.user)
 	bookmarks = {}
-	for category in categories:
-		bookmarks[category] = Bookmark.objects.filter(category=category)
 
-	context = {'user': request.user, 'categories': categories, 'bookmarks': bookmarks}
+	raw_col_0 = categories.filter(column_number=0).order_by('row_number')
+	col_0 = []
+	for category in raw_col_0:
+		col_0.append([category, Bookmark.objects.filter(category=category)])
+
+	raw_col_1 = categories.filter(column_number=1).order_by('row_number')
+	col_1 = []
+	for category in raw_col_1:
+		col_1.append([category, Bookmark.objects.filter(category=category)])
+	
+	raw_col_2 = categories.filter(column_number=2).order_by('row_number')
+	col_2 = []
+	for category in raw_col_2:
+		col_2.append([category, Bookmark.objects.filter(category=category)])
+	
+	raw_col_3 = categories.filter(column_number=3).order_by('row_number')
+	col_3 = []
+	for category in raw_col_3:
+		col_3.append([category, Bookmark.objects.filter(category=category)])
+	
+	raw_col_4 = categories.filter(column_number=4).order_by('row_number')
+	col_4 = []
+	for category in raw_col_4:
+		col_4.append([category, Bookmark.objects.filter(category=category)])
+	
+	raw_col_5 = categories.filter(column_number=5).order_by('row_number')
+	col_5 = []
+	for category in raw_col_5:
+		col_5.append([category, Bookmark.objects.filter(category=category)])
+	
+
+
+	context = {
+				'user': request.user,
+				'categories': categories,
+				'columns': [col_0, col_1, col_2, col_3, col_4, col_5], 
+				# 'col_0': col_0, 
+				# 'col_1': col_1,
+				# 'col_2': col_2, 
+				# 'col_3': col_3, 
+				# 'col_4': col_4, 
+				# 'col_5': col_5,
+			}
 	# print(context)
 	return render(request, 'home.html', context)
 
