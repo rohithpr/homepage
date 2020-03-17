276d11983384a18275c624dcdbd50da4975de41b
Rohith <praroh2@gmail.com>
2020-03-17 11:55:53 +0530

    Configure sqlite for local development

--- homepage/settings/local.py
+++ homepage/settings/local.py
@@ -3,3 +3,10 @@ from .base import *
 ALLOWED_HOSTS = ["127.0.0.1"]
 
 DEBUG = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
+}
