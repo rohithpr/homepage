97f28aeac778ec7143971122dec5191f64573b64
Rohith <praroh2@gmail.com>
2020-03-17 13:36:59 +0530

    Use postgres locally, and rationalize settings

--- homepage/settings/base.py
+++ homepage/settings/base.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -38,6 +38,16 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+
+THIRD_PARTY_APPS = [
+    "rest_framework",
+]
+
+LOCAL_APPS = [
+    "bookmarks.apps.BookmarksConfig",
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
--- homepage/settings/local.py
+++ homepage/settings/local.py
@@ -4,9 +4,15 @@ ALLOWED_HOSTS = ["127.0.0.1"]
 
 DEBUG = True
 
+THIRD_PARTY_APPS = ["django_extensions"]
+
+INSTALLED_APPS += THIRD_PARTY_APPS
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "ATOMIC_REQUESTS": True,
+        "AUTOCOMMIT": True,
+        "NAME": "homepage",
     }
 }
