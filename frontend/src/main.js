7bb9c4f55bd8732a65b806496d0e4a972f4ddbab
Rohith PR <praroh2@gmail.com>
2020-06-02 23:41:27 +0530

    Revert "Setup element-ui"
    This reverts commit 6d454b4296454ea63113d519d5d922ebc5828be1.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,9 @@
-import Element from 'element-ui'
 import Vue from 'vue'
-
 import App from './App.vue'
 import store from './store'
 
 import './css/style.css';
 
-Vue.use(Element)
 Vue.config.productionTip = false
 
 new Vue({
